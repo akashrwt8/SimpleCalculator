59c32e23c0f26e24a7d80c7d429ed22dbd7a3e1a
akashrwt8 <akashsinghrawat80@gmail.com>
2024-04-14 08:20:22 +0530

    Added Util.h

---
+++ Util.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Addition
+
+int add (int a, int b) {
+    return a + b;
+}
+
+int add (int a, int b, int c) {
+    return a + b + c;
+}
+
+float add (float a, float b) {
+    return a + b;
+}
+// Subtraction 
+
+int sub (int a, int b) {
+    return a + b;
+}
+
+float sub (float a, float b) {
+    return a + b;
+}
