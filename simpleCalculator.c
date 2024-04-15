1eaf3dfdf628d03a7c2880ec35910c5f1a0d867c
akashrwt8 <akashsinghrawat80@gmail.com>
2024-04-15 22:10:23 +0530

    New commit

--- simpleCalculator.c
+++ simpleCalculator.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main() {
+        int a, b, c;
+        do
+        {
+            int choice;
+            printf("Menu :\n");
+            printf("1 Adding two numbers\n");
+            printf("1 Adding two numbers\n");
+            printf("1 Adding two numbers\n");
+            switch (choice)
+            {
+            case 1:
+                add()
+                break;
+            
+            default:
+                break;
+            }
+        } while (choice != 0);
         
     return 0;
 }
