1add625b1ee9279187429812eb7f03cd9bb3d84b
akashrwt8 <akashsinghrawat80@gmail.com>
2024-04-15 22:44:53 +0530

    Added addition function

--- calulator/Util.h
+++ calulator/Util.h
@@ -6,19 +6,9 @@ int add (int a, int b) {
     return a + b;
 }
 
-int add (int a, int b, int c) {
-    return a + b + c;
-}
-
-float add (float a, float b) {
-    return a + b;
-}
 // Subtraction 
 
 int sub (int a, int b) {
-    return a + b;
+    return a - b;
 }
 
-float sub (float a, float b) {
-    return a + b;
-}
--- calulator/simpleCalculator.c
+++ calulator/simpleCalculator.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
+#include "Util.h"
 
 int main() {
-        int a, b, c;
+        int choice;
         do
         {
-            int choice;
             printf("Menu :\n");
             printf("1 Adding two numbers\n");
-            printf("1 Adding two numbers\n");
-            printf("1 Adding two numbers\n");
+            printf("2 Adding two float numbers\n");
+            printf("3 Adding two numbers\n");
+            printf("0 Ending program\n");
+            printf("Enter a choice : ");
+            scanf("%d", &choice);
             switch (choice)
             {
-            case 1:
-                add()
+            case 1:{
+                int x, y;
+                printf("Enter a number : ");
+                scanf("%d", &x);
+                printf("Enter a number : ");
+                scanf("%d", &y);
+                printf("Addition of two number is %d\n", add(x, y));
                 break;
-            
+            }
             default:
+                if (choice != 0)
+                {
+                    printf("Invalid input\n");
+                }
                 break;
             }
         } while (choice != 0);
